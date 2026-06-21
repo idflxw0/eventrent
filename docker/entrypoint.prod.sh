db714448b9e4995e70a00d5e88fccfea8084cfbb
flow <idfl0wxzero@gmail.com>
2026-06-21 21:59:06 +0000

    feat: updated prod files

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "==> Warming up Symfony cache (prod)..."
 APP_ENV=prod php bin/console cache:warmup --no-debug
+chown -R www-data:www-data var/
 
 echo "==> Running migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
